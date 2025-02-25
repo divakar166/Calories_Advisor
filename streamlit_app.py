19908019e3d54b0619adb23c2fe2189601cb8683
Atul Sharma <87241497+atul120212@users.noreply.github.com>
2025-02-25 17:01:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-os.system("pip install google-generativeai")
+os.system("pip install google-generativeai pyyaml")
 import google.generativeai as genai
 
 from dotenv import load_dotenv
